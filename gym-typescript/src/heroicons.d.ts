3f0e55a7ffa32b7773d8128ce9e7372960c87c1c
H4armin <bhankhariyaharmin294@gmial.com>
2023-11-14 01:29:28 +0530

    first commit

---
+++ gym-typescript/src/heroicons.d.ts
@@ -0,0 +1,6 @@
+// src/heroicons.d.ts
+declare module '@heroicons/react' {
+    export * from '@heroicons/react/outline';
+    export * from '@heroicons/react/solid';
+  }
+  
---
+++ gym-typescript/src/shared/types.ts
@@ -0,0 +1,12 @@
+export enum SelectedPage {
+    Home = "home",
+    Benefits = "benefits",
+    OurClasses = "ourClasses",
+    ContactUs = "contactus"
+}
+
+export interface BenefitType {
+    icon: JSX.Element;
+    title: string;
+    description : string;
+}
